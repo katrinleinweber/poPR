391701bf7e8d2582481f70d5d67bd3a7609cfdd7
Katrin Leinweber <kalei@posteo.de>
2020-02-06 21:13:34 +0100

    Get URLs of all possible comparisons
    repo:default to all forks:branches

---
+++ tests/test_get_diff_urls.py
@@ -0,0 +1,9 @@
+from vipr.get_diff_urls import get_diff_urls
+
+
+def test_get_diff_url():
+    repo: str = 'katrinleinweber/ViPR-test'
+    diff_urls = get_diff_urls(repo)
+    compare_base = 'https://github.com/' + repo + '/compare/master...'
+    for head in ['ViPR-test-2:dev', 'ViPR-test-2:no-pr', 'ViPR-test-1:patch']:
+        assert compare_base + head in diff_urls
--- vipr/__init__.py
+++ vipr/__init__.py
@@ -1,3 +1,4 @@
 __version__ = '0.1.0'
 
+from vipr.get_diff_urls import get_diff_urls
 from vipr.get_pr_branches import get_pr_branches
