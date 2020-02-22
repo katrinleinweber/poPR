6639f45f7d3099c1b27c18d17833c2d366226d3f
Katrin Leinweber <kalei@posteo.de>
2020-02-22 12:52:14 +0100

    Rename & add matching quote

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# ViPR: Virtual PR Review Tool
+# poPR: Find potential Pull Requests
+
+> I believe it is worthwhile trying to discover more about the
+> [branch-iverse], even if this only teaches us how little we
+> [have merged].
+>
+> -- liberally quoted from: [Karl Popper's _Conjectures and Refutations: The Growth of Scientific Knowledge_ (1963)](https://en.wikiquote.org/wiki/Karl_Popper#Conjectures_and_Refutations:_The_Growth_of_Scientific_Knowledge_(1963))
 
 This is tool helps maintainers observe the fork-iverse around a given project/repo.
 It finds forks and branches that could be pull-requested by their authors, but haven't.
---
+++ tests/test_popr.py
@@ -0,0 +1,22 @@
+from github import Github
+
+from popr import __version__
+from popr.construct_base_url import construct_base_url
+from popr.extract_branches import extract_branches
+
+origin: str = "katrinleinweber/poPR"
+repo: object = Github().get_repo(origin)
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_extract_branches():
+    assert extract_branches(repo, from_pulls=True) == {"branch-in-pr-1", "branch-in-pr-2"}
+    assert extract_branches(repo) == {'dev', 'master'}
+
+
+def test_construct_base_url():
+    base_url = construct_base_url(repo)
+    assert base_url == "https://github.com/katrinleinweber/poPR/compare/master...{head}"
