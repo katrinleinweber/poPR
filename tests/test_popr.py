518ac386ba19014d75c8d4e64d54686558df49b5
Katrin Leinweber <kalei@posteo.de>
2020-02-22 14:28:37 +0100

    Actually correct branch name in test (see #4)

--- tests/test_popr.py
+++ tests/test_popr.py
@@ -14,7 +14,7 @@ def test_version():
 
 def test_extract_branches():
     assert extract_branches(repo, from_pulls=True) == {"branch-in-pr-1", "branch-in-pr-2"}
-    assert extract_branches(repo) == {'TEST-intra-repo-pr', 'master'}
+    assert extract_branches(repo) == {'master'}
 
 
 def test_construct_base_url():
