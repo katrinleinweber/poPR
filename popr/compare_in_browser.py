db227954344430761f893274601460c41c0c0dce
Katrin Leinweber <kalei@posteo.de>
2020-02-23 16:27:55 +0100

    Remove completed TODO
    See e06f856 / "Rewrite based on pseudocode" in main.py

--- popr/compare_in_browser.py
+++ popr/compare_in_browser.py
@@ -3,7 +3,6 @@ import webbrowser
 
 def compare_in_browser(to_compare: str, base_url: str):
     url = base_url.format(head=to_compare)
-    # TODO: Wait for user input to continue
     # Open compare URL in browser
     # learned from https://stackoverflow.com/a/7062272/4341322
 
